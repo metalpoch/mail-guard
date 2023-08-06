6df5045f91d8e85ffa8878c78fdd3fae8e5357ab
Keiber Urbila <keiberup.dev@gmail.com>
2023-08-06 11:36:16 -0400

    style: landing page now is "responsive"

--- src/components/Home/Testimonial/index.tsx
+++ src/components/Home/Testimonial/index.tsx
@@ -1,33 +1,62 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import style from "./Testimonial.module.css";
 import clients from "@/mocks/clients";
 import type { Client } from "@/lib/components.types";
 
 export default function Testimonial() {
+  const [testimonialID, setTestimonialID] = useState(0);
+  const [animation, setAnimation] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setAnimation("animate__fadeInDown"),
+      5000
+    );
+    return () => clearInterval(interval);
+  }, [testimonialID]);
+
+  useEffect(() => {
+    if (animation === "animate__fadeInDown") {
+      const interval = setInterval(
+        () => setAnimation("animate__fadeOutDown animate__fast"),
+        4200
+      );
+      return () => clearInterval(interval);
+    }
+  }, [animation]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTestimonial = (testimonialID + 1) % clients.length;
+      setTestimonialID(newTestimonial);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [testimonialID]);
+
   return (
-    <section id="testimonials">
+    <section id="testimonials" className={style.wrapper}>
       <div className={style.testimonials}>
-        {clients.map((client: Client) => (
-          <div key={client.name} className={style.client}>
-            <Image
-              src={client.perfil}
-              height={163}
-              width={163}
-              alt={`Picture of ${client.name}`}
-            />
-            <div className={style.card}>
-              <p className={style.ranking}>{`${"⭐".repeat(
-                client.ranking,
-              )}`}</p>
-              <p className={style.body}>{client.message}</p>
-              <p className={style.name}>{client.name}</p>
-            </div>
+        <div className={`${style.client} animate__animated ${animation}`}>
+          <Image
+            src={clients[testimonialID].perfil}
+            height={163}
+            width={163}
+            alt={`Picture of ${clients[testimonialID].name}`}
+          />
+          <div className={style.card}>
+            <p className={style.ranking}>{`${"⭐".repeat(
+              clients[testimonialID].ranking
+            )}`}</p>
+            <p className={style.body}>{clients[testimonialID].message}</p>
+            <p className={style.name}>{clients[testimonialID].name}</p>
           </div>
-        ))}
+        </div>
       </div>
-      <div className="description">
+      <div className={style.description}>
         <h2>
           ¿Quieres verificar los correos electrónicos de tus contactos con
           seguridad y eficiencia?
