9188d9c589f6f03192b55264785bb55136effe6f
jesus <jesus_lopez1997@hotmail.com>
2023-08-06 15:31:03 -0400

    fix: add profile prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Home from "@/components/Home";
+import { Profile } from "@/lib/api.types";
 import { createServerSupabaseClient } from "@/lib/supabase/supabase-server";
 
 export default async function HomePage() {
@@ -8,9 +9,15 @@ export default async function HomePage() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select()
+    .eq("user_id", user?.id)
+    .single();
+
   return (
     <>
-      <Home user={user} />
+      <Home user={user} profile={profile as Profile} />
     </>
   );
 }
