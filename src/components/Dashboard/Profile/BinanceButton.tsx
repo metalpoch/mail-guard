c262004345a5e3f4b6bcf0a0763f89b5aaf9814c
jesus <jesus_lopez1997@hotmail.com>
2023-08-15 17:27:58 -0400

    modify: add funds button icon

--- src/components/Dashboard/Profile/BinanceButton.tsx
+++ src/components/Dashboard/Profile/BinanceButton.tsx
@@ -1,23 +1,20 @@
+import Link from "next/link";
 import style from "./Profile.module.css";
 
 export default function BinanceButton() {
   return (
-    <a
-      onClick={() => alert("redireccionando a pagame-mis-billuyos/")}
-      className={style.binanceButton}
-    >
+    <Link href="/add-founds" className={style.binanceButton}>
       +
       <svg
+        className="w-6 h-6 text-gray-800 dark:text-white"
+        aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
+        fill="currentColor"
+        viewBox="0 0 20 20"
       >
-        <path
-          fill="currentColor"
-          d="m16.624 13.92l2.717 2.716l-7.353 7.353l-7.352-7.352l2.717-2.717l4.636 4.66l4.635-4.66zm4.637-4.636L24 12l-2.715 2.716L18.568 12l2.693-2.716zm-9.272 0l2.716 2.692l-2.717 2.717L9.272 12l2.716-2.715zm-9.273 0L5.41 12l-2.692 2.692L0 12l2.716-2.716zM11.99.01l7.352 7.33l-2.717 2.715l-4.636-4.636l-4.635 4.66l-2.717-2.716L11.989.011z"
-        />
+        <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z" />
+        <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z" />
       </svg>
-    </a>
+    </Link>
   );
 }
